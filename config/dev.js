bc0e09f12500fbf7c9f517cd88cb1d5b57f7a0c1
MrLiem <liem12t@gmail.com>
2020-11-05 14:39:18 +0700

    Video4U Server V1

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://mrliem:Liemkg123@cluster0.rzlhr.mongodb.net/YoutubeClone?retryWrites=true&w=majority",
+};
---
+++ models/Video.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+const moment = require("moment");
+const Schema = mongoose.Schema;
+
+const videoSchema = mongoose.Schema(
+  {
+    writer: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      maxlength: 50,
+    },
+    description: {
+      type: String,
+    },
+    privacy: {
+      type: Number,
+    },
+    filePath: {
+      type: String,
+    },
+    category: String,
+    views: {
+      type: Number,
+      default: 0,
+    },
+    duration: {
+      type: String,
+    },
+    thumbnail: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Video = mongoose.model("Video", videoSchema);
+
+module.exports = { Video };
